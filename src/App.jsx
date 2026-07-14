c522a3c42d09ebf9f18cdb6add43c37945fb9d97
sajana-46 <sajanaskuttiyil@gmail.com>
2026-07-14 14:59:46 +0530

    view lab completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Addcourse from './components/Addcourse'
+import Viewlab from './components/Viewlab'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <Addcourse/>
+    <Viewlab/>
       
     </>
   )
---
+++ src/components/Viewlab.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import{useEffect} from 'react'
+
+
+
+
+const Viewlab = () => {
+    const[data,changeData]=useState([])
+
+
+    const fetchData = () =>{
+     axios.get("http://localhost:3000/view-all").then(
+        (res)=>{
+            changeData(res.data)
+
+        }
+    ).catch()   
+
+
+    }
+    useEffect(()=>{
+        fetchData()
+    },[])
+    
+  return (
+    <div>
+        
+        
+        <div className="container mt-4">
+        <h2 className="text-center mb-4">View All</h2>
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Department</th>
+              <th>Sem</th>
+              <th>Course</th>
+              <th>System Number</th>
+              <th>Login Time</th>
+              <th>Logout Time</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {data.map(
+                (value,index)=>{
+                    return(
+                         <tr>
+                        <td>{value.name}</td>
+                        <td>{value.department}</td>
+                        <td>{value.sem}</td>
+                        <td>{value.course}</td>
+                        <td>{value.systemNumber}</td>
+                        <td>{value.loginTime}</td>
+                        <td>{value.logoutTime}</td>
+                        <td>{value.date}</td>
+                        </tr>
+                    )
+                }
+            )
+                
+            }
+           
+             </tbody>
+        </table>
+      </div>
+
+        
+
+
+    </div>
+  )
+}
+
+export default Viewlab
