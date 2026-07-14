4aeafde4c3e3d8c808f0c78f886b1e203609740b
sajana-46 <sajanaskuttiyil@gmail.com>
2026-07-14 14:48:36 +0530

    add course completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Addcourse from './components/Addcourse'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Addcourse/>
       
     </>
   )
